dc078ee9647b20a267aa0d7bab7b82eac43007ca
Taylan Karaman <taylan.karaman@gmail.com>
2017-10-09 17:11:25 +1100

    Do Initial Migration

---
+++ ExpenseApplication/Migrations/20171009060626_InitialMigration.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ExpenseApplication.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExpenseForm",
+                columns: table => new
+                {
+                    ExpenseFormId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RejectReason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    State = table.Column<int>(type: "int", nullable: false),
+                    Total = table.Column<decimal>(type: "decimal(18, 2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseForm", x => x.ExpenseFormId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Expense",
+                columns: table => new
+                {
+                    ExpenseId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    ExpenseFormId = table.Column<int>(type: "int", nullable: true),
+                    ItemDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReceiptDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReceiptNumber = table.Column<int>(type: "int", nullable: false),
+                    ReimbursementAmount = table.Column<decimal>(type: "decimal(18, 2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Expense", x => x.ExpenseId);
+                    table.ForeignKey(
+                        name: "FK_Expense_ExpenseForm_ExpenseFormId",
+                        column: x => x.ExpenseFormId,
+                        principalTable: "ExpenseForm",
+                        principalColumn: "ExpenseFormId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expense_ExpenseFormId",
+                table: "Expense",
+                column: "ExpenseFormId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Expense");
+
+            migrationBuilder.DropTable(
+                name: "ExpenseForm");
+        }
+    }
+}
